ce9778b4b0ccc4025b80a9b90f1978d13a2b5270
Juan Pedro <valenciagomezjuanpedro82@gmail.com>
2019-11-26 09:47:26 -0500

    sistHeladeria version 1.0

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<script src='js/jquery.js' type='text/javascript'></script>
+		<script src='js/bootstrap.min.js' type='text/javascript'></script>
+		<link href='css/css.css' rel='stylesheet' type='text/css'>
+		<link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+		<link href='css/bootstrap-theme.min.css' rel='stylesheet' type='text/css'>
+		<div class="row">
+		<title>Tienda de helados </title>
+	</head>
+	
+	<body>
+	<header>
+
+	<div class="container">
+	<form action="" class="form-horizontal">
+<div class="col-md-12">
+
+		<img src='' id='cab'>
+		<h1>Tienda de helados</h1>
+		<nav class="navbar navbar-inverse" role="navigation" id="supmenu">
+			<ul class="nav nav-pills">
+				<li class="active"><a href='index.php'>Inicio</a></li>
+				<li><a href='productos.php'>Productos</a></li>
+				<li class="dropdown" role="presentation"><a lass="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administrador</a>
+					<ul class="dropdown-menu">
+						<li><a href='registraprod.php'>RegistrarProducto</a></li>
+						<li><a href='eliminaprod.php'>EliminaProducto</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		</div>
+		</form>
+		</div>
+	</header>
+	
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+<div class="container">
+	<form action="" class="form-horizontal">
+<div class="col-md-8">
+	<article>
+			<h1>Bienvenido a la tienda </h1>
+	</article>
+	</div>
+	</form>
+	</div>
+<?php include 'footer.php'; ?>
+
